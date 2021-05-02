1a12ffdbacc0aa8da5e2a51eff761996b83f5221
shaheen5 <miyashaheen1995@gmail.com>
2021-05-02 15:45:04 +0530

    Added : Code To Allow Analyser To Load Indian State Census Info From CSV File

--- main/java/censusanalyser/CensusAnalyser.java
+++ main/java/censusanalyser/CensusAnalyser.java
@@ -1,7 +1,41 @@
 package censusanalyser;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
 public class CensusAnalyser {
-    public static void main(String[] args) {
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+
+    //method to load Indian State Census Information from csv file
+    public void loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(IndiaCensusCSV.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
+            int namOfEateries = 0;
+            while (censusCSVIterator.hasNext()) {
+                namOfEateries++;
+                IndiaCensusCSV censusData = censusCSVIterator.next();
+            }
+            System.out.println(namOfEateries);
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }
+    }
+
+    public static void main(String[] args) throws CensusAnalyserException {
         System.out.println("Welcome To Indian State Census Analyser Problem!");
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
     }
 }

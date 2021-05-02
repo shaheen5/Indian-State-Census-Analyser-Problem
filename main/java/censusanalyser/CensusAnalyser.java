ac2d2b5f47b3f97b47b98825ba65d7b0fa52d937
shaheen5 <miyashaheen1995@gmail.com>
2021-05-02 14:41:31 +0530

    Added : Code To Display Welcome Message

---
+++ main/java/censusanalyser/CensusAnalyser.java
@@ -0,0 +1,7 @@
+package censusanalyser;
+
+public class CensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Indian State Census Analyser Problem!");
+    }
+}
